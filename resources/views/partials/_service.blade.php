7f81d21e17c3442046aa9c614ce1898dbe2da093
Aditya Restu Adji <adit.restu77@gmail.com>
2023-01-23 22:54:24 +0700

    add images to service section

--- resources/views/partials/_service.blade.php
+++ resources/views/partials/_service.blade.php
@@ -3,28 +3,28 @@
 
     <div class="d-flex flex-wrap align-items-center justify-content-center">
       <div class="card m-3" style="width: 18rem;">
-          <img src="{{ asset('images/bannerimg.png') }}" class="card-img-top" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('images/videocall.jpg') }}" class="card-img-top" style="max-height: 12rem" alt="...">
+          <div class="card-body d-flex flex-column align-items-center justify-content-center">
             <h5 class="card-title fw-bold">Konseling Online</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text text-center">Konsultasikan dengan Psikolog Melalui Layanan Video Call atau Call.</p>
             <a href="/conseling" class="btn btn-violet">Jadwalkan Konseling</a>
           </div>
       </div>
 
       <div class="card m-3" style="width: 18rem;">
-          <img src="{{ asset('images/bannerimg.png') }}" class="card-img-top" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('images/phone-talk.jpg') }}" class="card-img-top" style="max-height: 12rem" alt="...">
+          <div class="card-body d-flex flex-column align-items-center justify-content-center">
             <h5 class="card-title fw-bold">Tanya Psikolog</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text text-center">Layanan Gratis yang bisa anda akses untuk bertanya tentang masalah mental anda.</p>
             <a href="/tanya-psikolog" class="btn btn-violet">Tanya Sekarang</a>
           </div>
       </div>
 
       <div class="card m-3" style="width: 18rem;">
-          <img src="{{ asset('images/bannerimg.png') }}" class="card-img-top" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('images/tesmental.jpg') }}" class="card-img-top" style="max-height: 12rem" alt="...">
+          <div class="card-body d-flex flex-column align-items-center justify-content-center">
             <h5 class="card-title fw-bold">Tes Mental</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text text-center">Membantu anda untuk mengetahui masalah mental apa yang sedang kamu hadapi.</p>
             <a href="/tes-mental" class="btn btn-violet">Tes Sekarang</a>
           </div>
       </div>
